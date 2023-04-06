c744c3fc56cda1e8bec2fba10b64d16c5082bf6b
Alexandre Sousa Silva <pseudoalexandre@gmail.com>
2023-04-06 14:00:40 -0300

    feat: add new items to localStorage

--- js/main.js
+++ js/main.js
@@ -1,14 +1,18 @@
 const $form = document.querySelector('#novoItem');
 const $list = document.querySelector('#list');
+const itemsInLocalStorage = [];
 
 $form.addEventListener('submit', (e) => addItemToTravelList(e));
 
 function addItemToTravelList(e) {
   e.preventDefault();
 
-  const itemName = e.target.elements['nome'].value;
-  const itemAmount = e.target.elements['quantidade'].value;
-  createListItem(itemName, itemAmount);
+  let itemName = e.target.elements['nome'];
+  let itemAmount = e.target.elements['quantidade'];
+  createListItem(itemName.value, itemAmount.value);
+
+  itemName.value = '';
+  itemAmount.value = '';
 }
 
 function createListItem(name, amount) {
@@ -22,4 +26,11 @@ function createListItem(name, amount) {
   newItem.innerHTML += name;
 
   $list.appendChild(newItem);
+
+  const currentItem = {
+    name: name,
+    amount: amount,
+  };
+  itemsInLocalStorage.push(currentItem);
+  localStorage.setItem('travelItems', JSON.stringify(itemsInLocalStorage));
 }
