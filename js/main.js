d863eb70692d1527aa5ae1de0de04605cf2891b8
Alexandre Sousa Silva <pseudoalexandre@gmail.com>
2023-04-08 01:19:07 -0300

    feat: remove item on localStorage and DOM

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ $form.addEventListener('submit', function addItemToBackpack(e) {
   let itemName = e.target.elements['nome'];
   let itemAmount = e.target.elements['quantidade'];
   const currentItem = {
-    id: itemsInLocalStorage.length,
+    id: '',
     name: itemName.value,
     amount: itemAmount.value,
   };
@@ -22,9 +22,19 @@ $form.addEventListener('submit', function addItemToBackpack(e) {
 
   if (isADuplicateItem) {
     currentItem.id = isADuplicateItem.id;
-    itemsInLocalStorage[currentItem.id].amount = currentItem.amount;
-    updateItemIntoDOM(currentItem);
+    const indexOfDuplicate = itemsInLocalStorage.findIndex(
+      (item) => item.id === isADuplicateItem.id
+    );
+    itemsInLocalStorage[indexOfDuplicate].amount = currentItem.amount;
+    updateItemIntoDOM(currentItem, indexOfDuplicate);
   } else {
+    if (itemsInLocalStorage.length === 0) {
+      currentItem.id = 0;
+    } else {
+      currentItem.id =
+        itemsInLocalStorage[itemsInLocalStorage.length - 1].id + 1;
+    }
+
     createItemIntoDOM(currentItem);
     itemsInLocalStorage.push(currentItem);
   }
@@ -47,11 +57,33 @@ function createItemIntoDOM(item) {
 
   newItem.appendChild(quantityOfItems);
   newItem.innerHTML += item.name;
+  newItem.appendChild(createDeleteButton(item.id));
 
   $list.appendChild(newItem);
 }
 
-function updateItemIntoDOM(item) {
-  const amount = $list.querySelectorAll('.item')[item.id].firstChild;
+function updateItemIntoDOM(item, index) {
+  const amount = $list.querySelectorAll('.item')[index].firstChild;
   amount.textContent = item.amount;
+}
+
+function createDeleteButton(id) {
+  const deleteBtn = document.createElement('button');
+  deleteBtn.textContent = 'X';
+
+  deleteBtn.addEventListener('click', (e) => {
+    deleteItem(e.target.parentElement, id);
+  });
+
+  return deleteBtn;
+}
+
+function deleteItem(element, id) {
+  itemsInLocalStorage.splice(
+    itemsInLocalStorage.findIndex((item) => item.id === id),
+    1
+  );
+
+  element.remove();
+  localStorage.setItem('backpack', JSON.stringify(itemsInLocalStorage));
 }
